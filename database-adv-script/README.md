d1cb75af633369b9287f108b84a8be03543156f9
Namisi-Aaron <aaronnamisi@gmail.com>
2025-07-02 12:35:13 +0300

    joins_queries.sql and directory readme files created

---
+++ database-adv-script/README.md
@@ -0,0 +1,3 @@
+This directory contains sql files for the ALX project: *Unleashing Advanced Querying Power*
+
+It is a part of the *ALX Airbnb Database Module*, where I implement advanced SQL querying and optimization techniques to work with a simulated Airbnb database
--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,26 @@
--- Retrieves all bookings and the respective users who made those bookings
-SELECT * FROM Booking a INNER JOIN User b ON a.user_id = b.user_id;
+-- Retrieves all bookings (booking_id) and the respective users who made those bookings (their full name)
 
+SELECT
+	a.booking_id,
+	first_name || ' ' || b.last_name guest_name
+FROM airbnb_clone_0."Booking" a
+INNER JOIN airbnb_clone_0."User" b
+ON a.user_id = b.user_id;
+
+-- Retrieves all properties (property_id) and their reviews (rating & comment), including properties that have no reviews.
+
+SELECT
+	a.property_id,
+	b.rating,
+	b.comment
+FROM airbnb_clone_0."Property" a
+LEFT JOIN airbnb_clone_0."Review" b
+ON a.property_id = b.property_id;
+
+-- Retrieves all users and all bookings (all records), even if the user has no booking or a booking is not linked to a user.
+
+SELECT DISTINCT
+	*
+FROM airbnb_clone_0."User" a
+FULL OUTER JOIN airbnb_clone_0."Booking" b
+ON a.user_id = b.user_id
