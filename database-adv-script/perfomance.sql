eb5928bfea6c6df8ca6d8cfcbd9bd52e34e7f55f
Namisi-Aaron <aaronnamisi@gmail.com>
2025-07-05 13:20:38 +0300

    Optimize Complex Queries

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -2,13 +2,15 @@
 SELECT * FROM airbnb_clone_0."Booking" b
 LEFT JOIN airbnb_clone_0."User" u ON b."user_id" = u."booking_id"
 LEFT JOIN airbnb_clone_0."Property" p ON b."property_id" = p."booking_id"
-LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id";
+LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id"
+WHERE b.booking_date >= '2022-01-01' AND b.booking_date <= '2022-12-31';
 
 -- This query analyzes the above query's performance
 EXPLAIN ANALYZE SELECT * FROM airbnb_clone_0."Booking" b
 LEFT JOIN airbnb_clone_0."User" u ON b."user_id" = u."booking_id"
 LEFT JOIN airbnb_clone_0."Property" p ON b."property_id" = p."booking_id"
-LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id";
+LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id"
+WHERE b.booking_date >= '2022-01-01' AND b.booking_date <= '2022-12-31';
 
 -- Refactored query
 SELECT
@@ -25,7 +27,8 @@ SELECT
 FROM airbnb_clone_0."Booking" b
 LEFT JOIN airbnb_clone_0."User" u ON b."user_id" = u."booking_id"
 LEFT JOIN airbnb_clone_0."Property" p ON b."property_id" = p."booking_id"
-LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id";
+LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id"
+WHERE b.booking_date >= '2022-01-01' AND b.booking_date <= '2022-12-31';
 
 -- Analisys of refactored query
 EXPLAIN ANALYZE SELECT
@@ -42,5 +45,5 @@ EXPLAIN ANALYZE SELECT
 FROM airbnb_clone_0."Booking" b
 LEFT JOIN airbnb_clone_0."User" u ON b."user_id" = u."booking_id"
 LEFT JOIN airbnb_clone_0."Property" p ON b."property_id" = p."booking_id"
-LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id";
-    
+LEFT JOIN airbnb_clone_0."Payment" pay ON b."booking_id" = pay."booking_id"
+WHERE b.booking_date >= '2022-01-01' AND b.booking_date <= '2022-12-31';
